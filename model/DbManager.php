752b1f8f254fdd9feda51a2e6997340a3192ba90
MrHD586 <34337152+MrHD586@users.noreply.github.com>
2018-02-22 11:17:18 +0100

    Requêtes test
    Requêtes de test sur la table articles.

--- model/DbManager.php
+++ model/DbManager.php
@@ -7,7 +7,8 @@ class DbManager {
 	private $password = "projet151";
 	private $dbname = "projet151";
 	
-	private $conn = null;
+	private $dbGh = null;
+
 	
 	function __construct() {
 		$this->Connect();
@@ -16,14 +17,14 @@ class DbManager {
 	
 	// connexion à la db
 	public function Connect() {
-		$this->conn = new PDO("mysql:host=$this->servername;dbname=$this->dbname", $this->username, $this->password);
+		$this->dbGh = new PDO("mysql:host=$this->servername;dbname=$this->dbname", $this->username, $this->password);
 		// set the PDO error mode to exception
-		$this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		$this->dbGh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	}
 	
 	// execute une requête
 	public function Query($query) {
-		$stmt = $this->conn->prepare($query);
+		$stmt = $this->dbGh->prepare($query);
 		$stmt->execute();
 		return $stmt;
 	}
--- view/article.php
+++ view/article.php
@@ -1,12 +1,26 @@
 <?php
+	include("../model/DbManager.php");
 	include("./partial/header.php");
 	
-	$sql = "SELECT * FROM t-articles"; 
-	$resultat = $dbmanager->Query($sql);
-	$resultat = $resultat->fetch();
-	
-	
-	
+	$sql = "SELECT * FROM t_articles"; 
+	$dbManager = new DbManager();
+	$resultat = $dbManager->Query($sql);
+
+	if ($resultat->rowCount() > 0) {
+		foreach ($resultat->fetchAll() as $row) { ?>
+			 <tr>
+    			<td><?php echo 'id '.$row["idArticle"];?></td><br/>
+				<td><?php echo 'Nom '.$row["AName"];?></td><br/>
+				<td><?php echo 'Stock '.$row["AStock"];?></td><br/>
+				<td><?php echo 'Prix '.$row["APrix"];?></td><br/>  
+				<td><?php echo 'Description '.$row["ADescription"];?></td><br/>  
+				<td><?php echo 'Catégorie '.$row["Fk_Categories"];?></td><br/>  
+				<td><?php echo 'Marque '.$row["Fk_Marque"];?></td><br/>  
+				<td><?php echo 'Pic Article '.$row["Fk_PicArtivles"];?></td><br/>  
+  			</tr>
+			<br/>
+		<?php }
+	}
 	
 	
 	

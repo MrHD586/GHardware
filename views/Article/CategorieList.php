d2456d6d413896e9b6ced1a7f7fec114cb2bafbb
Valentin <vfavre@live.fr>
2018-03-15 11:57:32 +0100

    cat
    cat

--- views/Article/CategorieList.php
+++ views/Article/CategorieList.php
@@ -15,77 +15,11 @@ foreach($Categoriearticles as $value){
     echo'</br>';
 }
 
-?>
+echo'
 			
 			<div class="col-xs-12 col-sm-6 col-md-4">
 			<!-- START ARTICLE-->	
 				<div class="articlebox">
-					<a id="categorylink" href="#">
-						<!-- Category -->
-							<div class="col-xl-12">
-								Catégorie
-							</div>
-						
-						<!-- Image -->
-							<div class="col-xl-12">
-								<img class="img-responsive" src="images/imagetemplate.png">
-							</div>
-						<!-- Price and Text -->
-							<div class="col-xl-12">
-								214.50
-							</div>
-							<div class="col-xl-12">
-								NOM ARTICLE
-							</div>
-							
-							</a>	
-							
-						<!-- Notation and Cart -->
-							<div class="col-xl-12">
-								<div class="row">
-									<div class="col-xs-9">
-										*****
-									</div><div class="col-xs-3">
-										<a href="#"><img class="img-responsive" src="images/carticon.png"></a>
-									</div>
-								</div>
-							</div>
-					</div>
-					
-					<div class="articlebox">
-					<a id="categorylink" href="#">
-						<!-- Category -->
-							<div class="col-xl-12">
-								Catégorie
-							</div>
-						
-						<!-- Image -->
-							<div class="col-xl-12">
-								<img class="img-responsive" src="images/imagetemplate.png">
-							</div>
-						<!-- Price and Text -->
-							<div class="col-xl-12">
-								214.50
-							</div>
-							<div class="col-xl-12">
-								NOM ARTICLE
-							</div>
-							
-							</a>	
-							
-						<!-- Notation and Cart -->
-							<div class="col-xl-12">
-								<div class="row">
-									<div class="col-xs-9">
-										*****
-									</div><div class="col-xs-3">
-										<a href="#"><img class="img-responsive" src="images/carticon.png"></a>
-									</div>
-								</div>
-							</div>
-					</div>
-					
-					<div class="articlebox">
 					<a id="categorylink" href="#">
 						<!-- Category -->
 							<div class="col-xl-12">

764503d8ea521a90c9c284b4ce9c929584be23d0
Valentin <vfavre@live.fr>
2018-03-29 10:13:45 +0200

    Test Modif Responsive

--- views/Article/article.php
+++ views/Article/article.php
@@ -11,10 +11,10 @@
 foreach($articles as $articles){
 	echo '<div class="col-lg-9">
 		      <div class="row">
-                   <div class="col-lg-4 col-lg-offset-4"><h1>'.$articles['AName'].'</h1></div>
+                   <div class="col-xs-4 col-xs-offset-4"><h1>'.$articles['AName'].'</h1></div>
               </div>
     		  <div class="row">
-                  <div class="col-lg-7 col-xs-offset-1"><img class="img-responsive" src="images/imagetemplate.png"></div> 
+                  <div class="col-lg-7"><img class="img-responsive" src="images/imagetemplate.png"></div> 
     			  <div class="col-lg-4">
     			       <div class="col-md-12">En Stock : <span class="badge text-success">'.$articles['AStock'].'</span></div>
     				   <div class="col-md-12"><h2>Prix : <span class="badge text-success">'.$articles['APrix'].'</h2></span></div>';
--- views/header.php
+++ views/header.php
@@ -23,6 +23,7 @@
         <html>
           <head>
         	<meta charset="utf-8">
+			<meta name="viewport" content="width=device-width, initial-scale=1">
         	<title>Ghardware</title>
         	<link <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
             <script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>

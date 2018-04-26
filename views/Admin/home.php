c1edc931920dfd199138e44f3d2b17b9ae85248b
Valentin <vfavre@live.fr>
2018-04-26 11:37:03 +0200

    test
    test

--- views/Admin/home.php
+++ views/Admin/home.php
@@ -7,9 +7,87 @@
     ################################################################################
    
     
-   
+    echo'
+		
+	
+    	<div class="col-lg-9">
+			
+			
+			<!-- ADMIN -->
+			<div class="col-xs-12 col-sm-6 col-md-3">
+				
+				<div class="articlebox">
+					<a id="categorylink" href="#">
+					
+							<div class="col-xl-12">
+								<center>Nouvel Utilisateur</center>
+							</div>
+													<div class="col-xl-12">
+								<img class="img-responsive" src="images/adduser.png">
+							</div>
+							
+							</a>	
+							
+					</div>
+				</div>
+				
+			<div class="col-xs-12 col-sm-6 col-md-3">
+				
+				<div class="articlebox">
+					<a id="categorylink" href="#">
+					
+							<div class="col-xl-12">
+								<center>Nouvel Article</center>
+							</div>
+													<div class="col-xl-12">
+								<img class="img-responsive" src="images/article.png">
+							</div>
+							
+							</a>	
+							
+					</div>
+				</div>
+				
+			<div class="col-xs-12 col-sm-6 col-md-3">
+				
+				<div class="articlebox">
+					<a id="categorylink" href="#">
+					
+							<div class="col-xl-12">
+								<center>Nouvelle Marque</center>
+							</div>
+													<div class="col-xl-12">
+								<img class="img-responsive" src="images/mark.png">
+							</div>
+							
+							</a>	
+							
+					</div>
+				</div>
+				
+			<div class="col-xs-12 col-sm-6 col-md-3">
+				
+				<div class="articlebox">
+					<a id="categorylink" href="#">
+					
+							<div class="col-xl-12">
+								<center>Nouvelle Catégorie</center>
+							</div>
+													<div class="col-xl-12">
+								<img class="img-responsive" src="images/category.png">
+							</div>
+							
+							</a>	
+							
+					</div>
+				</div>
+				
+			<!-- FIN ADMIN -->
+			
+				</div>
+    ';
 
-
+?>
 
 
 

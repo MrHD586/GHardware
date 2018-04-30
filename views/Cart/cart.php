0973b75a7bae88e42ce613e06f83b3d0c3f51666
togmar910 <33432619+togmar910@users.noreply.github.com>
2018-04-30 18:33:43 +0200

    a

--- views/Cart/cart.php
+++ views/Cart/cart.php
@@ -8,11 +8,13 @@ echo'
 			<div class="row">
 				
 				<div class="cartmenu">
-                <form action="index.php?controller=Cart&action=SupprimerArticles" method="POST">
+                <form action="index.php?controller=User&action=creation&Paniercookie=1" method="POST">
 					<input type="submit" name="commande" value="Passer la commande">
+                </form>    
+                <form action="index.php?controller=Cart&action=SupprimerArticles" method="POST">
                     <input type="submit" name="vider" value="Vider le panier">
-				</div>
 				</form>
+                </div>
 			</div>
 		
 		<!-- ARTICLES -->';

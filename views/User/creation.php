eecbc8020c809b2cfa6363c15a7e979b3f4fafe8
togmar910 <33432619+togmar910@users.noreply.github.com>
2018-04-30 18:36:07 +0200

    a

--- views/User/creation.php
+++ views/User/creation.php
@@ -35,10 +35,12 @@
         $errorEmail = null;
     }
     
-    echo '
-        <h2>Création de votre compte</h2><br/>';
+   
+        
         if($_GET['Paniercookie']==1){
              echo '<h3>Veuillez-vous crée un compte avant de pouvoir commander ou connecter-vous</h3><br/>'; 
+        }else{
+            echo ' <h2>Création de votre compte</h2><br/>';
         }
 		echo'<form method="post" action="">
 		      '.$errorEmptyField.'

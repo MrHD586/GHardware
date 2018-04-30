e10687e9418bb8d8e3f6f346a781d0ed803e2491
togmar910 <33432619+togmar910@users.noreply.github.com>
2018-04-30 18:39:36 +0200

    a

--- controllers/Cart/CommanderCookie.php
+++
@@ -1,9 +0,0 @@
-<?php
-################################################################################
-#### Auteur : Viquerat Killian
-#### Date : 30 Avril 2018
-#### Page controllers/Cart/SupprimerArticles.php:
-#### Commande et redirection du panier au login
-################################################################################
-//redirection vers l'affichage des articles dans le panier
-header("location:index.php?controller=User&action=creation&Paniercookie=1");

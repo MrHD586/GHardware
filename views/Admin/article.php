da1a80de3a27b2deb48a6ec5d7e92021329dca22
MrHD586 <34337152+MrHD586@users.noreply.github.com>
2018-05-18 11:31:09 +0200

    Formulaire admin article

--- views/Admin/article.php
+++ views/Admin/article.php
@@ -7,7 +7,7 @@
     ################################################################################
     
     //titre du formulaire
-    $formTitle = "Gestion des articles";
+    $pageTitle = "Gestion des articles";
     
     //message lors de création réussite
     if($_SESSION['ar_CreationSucces'] != null){
@@ -16,30 +16,129 @@
         $ar_CreationSucces = null;
     }
     
-    //FORMULAIRE//
-    echo ' <h3>'.$formTitle.'</h3><br/>';
+    //------ Haut de page ------//
+    echo '
+        <div class="row">
+			<div class="col-xs-offset-1 col-lg-8">
+                <h3>'.$pageTitle.'</h3><br/>';
+    
+    
+    //------ TABLEAU ------//
+    echo '
+		        <div class="col-lg-12">
+			        <table id="tabadmin">
+        			  <tr>
+        				<th>ID</th>
+        				<th>Login</th>
+        				<th>Prénom</th>
+        				<th>Nom</th>
+        				<th>E-Mail</th>
+        				<th>Date de naissance</th>
+        				<th>Droits</th>
+        				<th>Actif</th>
+        			  </tr>
+        			  <tr>
+        				<td>1</td>
+        				<td>Login</td>
+        				<td>Prénom</td>
+        				<td>Nom</td>
+        				<td>E-Mail</td>
+        				<td>Date de naissance</td>
+        				<td>Droits</td>
+        				<td>Actif</td>
+        			  </tr>
+        			  <tr>
+        				<td>2</td>
+        				<td>Login</td>
+        				<td>Prénom</td>
+        				<td>Nom</td>
+        				<td>E-Mail</td>
+        				<td>Date de naissance</td>
+        				<td>Droits</td>
+        				<td>Actif</td>
+        			  </tr>
+        			  <tr>
+        				<td>3</td>
+        				<td>Login</td>
+        				<td>Prénom</td>
+        				<td>Nom</td>
+        				<td>E-Mail</td>
+        				<td>Date de naissance</td>
+        				<td>Droits</td>
+        				<td>Actif</td>
+        			  </tr>
+			        </table>
+		        </div> ';
+    
     
     //affichage des messages d'erreures contenus dans le tableau errorsArray
     foreach ($errorsArray as $key => $val) {
         echo '<p style="color:red;">'.$val.'</p>';
     }
     
-    echo'<form method="post" action="">
-                        '.$ar_CreationSucces.'
-                            
-                        <p>
-                			<label for="Name">Nom</label>
-                			<input type="text" name="Name" value="'.$formArticleNameValue.'"/>
-                		</p>
+    //------ FORMULAIRE ------//
+    echo'
+                <form method="post" action="">
+                      '.$ar_CreationSucces.'
+                        
+                      <p>
+                		<div class="col-lg-4"><label for="Name">Nom</label></div>
+                		<div class="col-lg-8"><input type="text" name="Name" value="'.$formArticleNameValue.'"/></div>
+                	  </p>
+
+                      <p>
+            			<div class="col-lg-4"><label for="Stock">Stock</label></div>
+            			<div class="col-lg-8"><input type="text" name="stock" value="'.$formArticleStockValue.'"/></div>
+            		  </p>
+
+                      <p>
+            			<div class="col-lg-4"><label for="Price">Prix</label></div>
+            			<div class="col-lg-8"><input type="text" name="price" value="'.$formArticlePriceValue.'"/></div>
+            		  </p>
+                      
+                      <p>
+            			<div class="col-lg-4"><label for="Description">Déscription</label></div>
+            			<div class="col-lg-8"><input type="text" name="description" value="'.$formArticleDescriptionValue.'"/></div>
+            		  </p>
+                      
+                      <p>
+            			<div class="col-lg-4"><label for="Category">Catégorie</label></div>
+            			<div class="col-lg-12">
+                            <select name="category">
+                                <option value="'.$formArticleCategoryValue.'">À remplir'.$formArticleCategoryValue.'</option>
+                            </select>
+                        </div>
+            		  </p>
+
+                      <p>
+            			<div class="col-lg-4"><label for="Brand">Marque</label></div>
+            			<div class="col-lg-12">
+                            <select name="Brand">
+                                <option value="'.$formArticleBrandValue.'">À remplir'.$formArticleBrandValue.'</option>
+                            </select>
+                        </div>
+            		  </p>
+
+                      <p>
+            			<div class="col-lg-4"><label for="PicArticle">Images</label></div>
+            			<div class="col-lg-12">
+                            <select name="PicArticle">
+                                <option value="'.$formArticlePicValue.'">À remplir'.$formArticlePicValue.'</option>
+                            </select>
+                        </div>
+            		  </p>
                 			    
-                        <p>
-            			    <label for="IsActive">Actif</label></br>
-            		        <input type="radio" name="isActive" value="1" checked="checked">Oui</input></br>
-                            <input type="radio" name="isActive" value="0">Non</input>
-            		    </p>
+                      <p>
+            			<div class="col-lg-4"><label for="IsActive">Actif</label></br></div>
+            		    <div class="col-lg-12"><input type="radio" name="isActive" value="1" checked="checked">Oui</input></br></div>
+                        <div class="col-lg-12"><input type="radio" name="isActive" value="0">Non</input></div>
+            		  </p>
                 			    
-                	    <p>
-                		    <input type="submit" name="submit" value="Envoyer"/>
-                	    </p>
-                	</form>
-                    ';
+                	  <p>
+                        <div class="col-lg-12"></div>
+                		<div class="col-xs-offset-2 col-lg-2"><input type="submit" name="submit" value="Envoyer"/></div>
+                	    <div class="col-lg-12"></div>
+                      </p>
+            	</form>
+            </div>
+        </div>';
--- views/Admin/category.php
+++ views/Admin/category.php
@@ -7,7 +7,7 @@
     ################################################################################
     
     //titre du formulaire
-    $formTitle = "Gestion des catégories";
+    $pageTitle = "Gestion des catégories";
     
     //message lors de création réussite
     if($_SESSION['cat_CreationSucces'] != null){
@@ -16,81 +16,86 @@
         $cat_CreationSucces = null;
     }
     
-    //FORMULAIRE//
-    echo ' <div class="row">
-		<div class="col-xs-offset-1 col-lg-8"> 
-		
-		<div class="col-lg-12">
-			<table id="tabadmin">
-			  <tr>
-				<th>ID</th>
-				<th>Login</th>
-				<th>Prénom</th>
-				<th>Nom</th>
-				<th>E-Mail</th>
-				<th>Date de naissance</th>
-				<th>Droits</th>
-				<th>Actif</th>
-			  </tr>
-			  <tr>
-				<td>1</td>
-				<td>Login</td>
-				<td>Prénom</td>
-				<td>Nom</td>
-				<td>E-Mail</td>
-				<td>Date de naissance</td>
-				<td>Droits</td>
-				<td>Actif</td>
-			  </tr>
-			  <tr>
-				<td>2</td>
-				<td>Login</td>
-				<td>Prénom</td>
-				<td>Nom</td>
-				<td>E-Mail</td>
-				<td>Date de naissance</td>
-				<td>Droits</td>
-				<td>Actif</td>
-			  </tr>
-			  <tr>
-				<td>3</td>
-				<td>Login</td>
-				<td>Prénom</td>
-				<td>Nom</td>
-				<td>E-Mail</td>
-				<td>Date de naissance</td>
-				<td>Droits</td>
-				<td>Actif</td>
-			  </tr>
-			</table>
-		</div>
-	
-			<h3>'.$formTitle.'</h3><br/>';
+    
+    //------ Haut de page ------//
+    echo '
+        <div class="row">
+			<div class="col-xs-offset-1 col-lg-8">
+                <h3>'.$pageTitle.'</h3><br/>';
+    
+    
+    //------ TABLEAU ------//
+    echo '
+		        <div class="col-lg-12">
+			        <table id="tabadmin">
+        			  <tr>
+        				<th>ID</th>
+        				<th>Login</th>
+        				<th>Prénom</th>
+        				<th>Nom</th>
+        				<th>E-Mail</th>
+        				<th>Date de naissance</th>
+        				<th>Droits</th>
+        				<th>Actif</th>
+        			  </tr>
+        			  <tr>
+        				<td>1</td>
+        				<td>Login</td>
+        				<td>Prénom</td>
+        				<td>Nom</td>
+        				<td>E-Mail</td>
+        				<td>Date de naissance</td>
+        				<td>Droits</td>
+        				<td>Actif</td>
+        			  </tr>
+        			  <tr>
+        				<td>2</td>
+        				<td>Login</td>
+        				<td>Prénom</td>
+        				<td>Nom</td>
+        				<td>E-Mail</td>
+        				<td>Date de naissance</td>
+        				<td>Droits</td>
+        				<td>Actif</td>
+        			  </tr>
+        			  <tr>
+        				<td>3</td>
+        				<td>Login</td>
+        				<td>Prénom</td>
+        				<td>Nom</td>
+        				<td>E-Mail</td>
+        				<td>Date de naissance</td>
+        				<td>Droits</td>
+        				<td>Actif</td>
+        			  </tr>
+			        </table>
+		        </div> ';
+    
     
     //affichage des messages d'erreures contenus dans le tableau errorsArray
     foreach ($errorsArray as $key => $val) {
         echo '<p style="color:red;">'.$val.'</p>';
     }
+    //------ FORMULAIRE ------//
+    echo'
+                <form method="post" action="">
+                    '.$cat_CreationSucces.'
+                        
+                    <p>
+            			<div class="col-lg-2"><label for="Name">Nom</label></div>
+            			<div class="col-lg-10"><input type="text" name="Name" value="'.$formCategoryNameValue.'"/></div>
+            		</p>
     
-    echo'<form method="post" action="">
-                '.$cat_CreationSucces.'
-                    
-                <p>
-        			<div class="col-lg-2"><label for="Name">Nom</label></div>
-        			<div class="col-lg-10"><input type="text" name="Name" value="'.$formCategoryNameValue.'"/></div>
-        		</p>
-
-                <p>
-    			    <div class="col-lg-2"><label for="IsActive">Actif</label></br></div>
-    		        <div class="col-lg-12"><input type="radio" name="isActive" value="1" checked="checked">Oui</input></br></div>
-                    <div class="col-lg-12"><input type="radio" name="isActive" value="0">Non</input></div>
-    		    </p>
-                    
-        	    <p>
-        		    <div class="col-lg-4"><input type="submit" name="submit" value="Envoyer"/></div>
-        	    </p>
-        	</form>
-			
+                    <p>
+        			    <div class="col-lg-2"><label for="IsActive">Actif</label></br></div>
+        		        <div class="col-lg-12"><input type="radio" name="isActive" value="1" checked="checked">Oui</input></br></div>
+                        <div class="col-lg-12"><input type="radio" name="isActive" value="0">Non</input></div>
+        		    </p>
+                        
+            	    <p>
+            		    <div class="col-xs-offset-2 col-lg-2"><input type="submit" name="submit" value="Envoyer"/></div>
+            	    </p>
+            	</form>
 			</div>
-				</div>
-            ';
+		</div>';
+    

29b4ba42c708caaa1917c5d3b70dec6a85ebcbab
Valentin <vfavre@live.fr>
2018-05-24 10:54:22 +0200

    Page Profil

---
+++ views/User/profile.php
@@ -0,0 +1,121 @@
+<?php
+    ################################################################################
+    #### Auteur : Valentin Favre
+    #### Date : 24 Mai 2018
+    #### Page de Profil 
+    ################################################################################
+echo '	
+	<div class="col-lg-9">
+			<div class="row">
+                       <div class="col-sm-3 col-xs-offset-1">
+							<div class="col-xs-12">
+								<img id="avatarmenu" width="135" height="135" src="images/defaultavatar.png">
+							</div>
+							
+							<div class="col-xs-12">
+								<a class="btn btn-default navbar-btn" href="#" role="button">Changer Avatar</a>
+							</div>
+					   </div>
+					   
+					   <div	class="col-xs-8">
+					   
+							<div class="col-sm-12">
+								<div class="col-sm-6">
+									<b>Nom d\'utilisateur : </b>
+								</div>
+								
+								<div class="col-sm-6">
+									Knighty23
+								</div>
+								
+								<div class="col-sm-12">
+									<a class="btn btn-default navbar-btn" href="#" role="button">Modifier</a>
+									<form method="post" action="#"><br><input class="searchbar" type="text" name="search" placeholder="Utilisateur"><button type="submit" class="btn btn-primary btn-sm">Valider</button></form>
+								</div>
+							
+								 
+							</div>
+							
+							<div class="col-sm-12">
+								<div class="col-sm-6">
+									<b>Nom </b>
+								</div>
+								
+								<div class="col-sm-6">
+									Favre
+								</div>
+								
+								<div class="col-sm-12">
+									<a class="btn btn-default navbar-btn" href="#" role="button">Modifier</a>
+									<form method="post" action="#"><br><input class="searchbar" type="text" name="search" placeholder="Nom"><button type="submit" class="btn btn-primary btn-sm">Valider</button></form>
+								</div>
+							
+								 
+							</div>
+							
+							<div class="col-sm-12">
+								<div class="col-sm-6">
+									<b>Prénom : </b>
+								</div>
+								
+								<div class="col-sm-6">
+									Valentin
+								</div>
+								
+								<div class="col-sm-12">
+									<a class="btn btn-default navbar-btn" href="#" role="button">Modifier</a>
+									<form method="post" action="#"><br><input class="searchbar" type="text" name="search" placeholder="Prénom"><button type="submit" class="btn btn-primary btn-sm">Valider</button></form>
+								</div>
+							
+								 
+							</div>
+							
+							<div class="col-sm-12">
+								<div class="col-sm-6">
+									<b>Date de Naissance : </b>
+								</div>
+								
+								<div class="col-sm-6">
+									23.01.1995
+								</div>
+								
+								<div class="col-sm-12">
+									<a class="btn btn-default navbar-btn" href="#" role="button">Modifier</a>
+									<form method="post" action="#"><input class="searchbar" type="date" name="date de naissance" placeholder="Utilisateur"><button type="submit" class="btn btn-primary btn-sm">Valider</button></form>
+								</div>
+							
+								 
+							</div>
+							
+							<div class="col-sm-12">
+								<div class="col-sm-12">
+									<b>Mot de passe : </b>
+								</div>
+								
+								<div class="col-sm-12">
+									<a class="btn btn-default navbar-btn" href="#" role="button">Modifier le mot de passe</a>
+									<form method="post" action="#">
+									<input class="searchbar" type="password" name="password" placeholder="Nouveau mot de passe">
+									<input class="searchbar" type="password" name="confirmpassword" placeholder="Confirmer nouveau mot de passe">
+									<button type="submit" class="btn btn-primary btn-sm">Valider</button></form>
+								
+								</div>
+							
+								 
+							</div>
+							
+							
+							
+							
+					   
+						   
+						   
+						   
+					   </div>
+			</div>
+	</div>
+	
+	';
+					   
+					   
+					   

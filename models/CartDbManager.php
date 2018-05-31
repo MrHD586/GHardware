de2d5bdff8be1937f15c8afbf4a183df9279ad0a
togmar910 <33432619+togmar910@users.noreply.github.com>
2018-05-31 20:33:14 +0200

    commencement commande

--- models/CartDbManager.php
+++ models/CartDbManager.php
@@ -40,9 +40,9 @@
             return $resultat->fetchAll();
         }
         
-        public function setNewPanier($Fk_Articles, $Fk_User, $PNombre) {
-            $sql = "INSERT INTO t_panier (Fk_Articles, Fk_User, PNombre)
-                        VALUES ('$Fk_Articles', '$Fk_User', '$PNombre')";
+        public function setNewPanier($Fk_Articles, $Fk_User, $PNombre, $isCommande) {
+            $sql = "INSERT INTO t_panier (Fk_Articles, Fk_User, PNombre, isCommande)
+                        VALUES ('$Fk_Articles', '$Fk_User', '$PNombre', '$isCommande')";
             $this->dbManager->Query($sql);
         }
         

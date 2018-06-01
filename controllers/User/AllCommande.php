78c663d0d4456a19305247585e30b38fab42c577
togmar910 <33432619+togmar910@users.noreply.github.com>
2018-06-01 19:41:52 +0200

    commande user

--- controllers/User/AllCommande.php
+++ controllers/User/AllCommande.php
@@ -1,2 +1,8 @@
 <?php
-    include 'views/User/commandelist.php';
+include 'models/CommandeManager.php';
+session_start();
+$commandeManager = new CommandeManager();
+$userLogin = $_SESSION['user_name'];
+$user = $commandeManager->getUserByLogin($userLogin);
+include 'views/aside.php';
+include 'views/User/commandelist.php';

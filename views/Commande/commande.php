c6565783c589012ead9df9f368203a5295ac9368
togmar910 <33432619+togmar910@users.noreply.github.com>
2018-06-01 15:05:29 +0200

    a

--- views/Commande/commande.php
+++ views/Commande/commande.php
@@ -5,8 +5,8 @@
 #### Page views/Commande/commande.php:
 #### 		  Page pour choisir le moyen de payement
 ################################################################################
-echo '<h1>Moyen de payement<h1>
-<p style="color:red;">L\'adresse de votre profile va être utiliser comme adresse de destination veuillez être sur qu\elle sois correcte avant de passer votre commande</p>
+echo '<h1>Moyen de payement</h1>
+<p style="color:red;">L\'adresse de votre profile va être utiliser comme adresse de destination veuillez être sur qu\'elle sois correcte avant de passer votre commande</p>
 <form action="index.php?controller=Commande&action=Commande" method="POST">
     <input type="radio" name="payment" value="0" checked>Nature<br>
     <input type="radio" name="payment" value="1" checked>Facture<br>

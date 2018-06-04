4482d6f3f0614b31df4fb678c46b4a80601831ae
Valentin <vfavre@live.fr>
2018-06-04 11:36:58 +0200

    Correction Addstyle

--- views/Site/home.php
+++ views/Site/home.php
@@ -16,55 +16,53 @@
 			
 			<div class="slideshow-container">
 
+			<!-- Slide 1 -->
 				<div class="mySlides fade">
-				<a href="#">
 				  <div class="numbertext">1 / 3</div>
-				  <img src="images/Slide1.png" style="width:100%">
+					<a href="http://ghardware.clanviquerat.ch/index.php?controller=Article&action=articlecommentaire&id=13"><img src="images/Slide1.png" style="width:100%"></a>
+				  <div class="text">1</div>
 				</div>
-
+			
+			<!-- Slide 2 -->
 				<div class="mySlides fade">
-				<a href="#1">
 				  <div class="numbertext">2 / 3</div>
-				  <img src="images/Slide2.png" style="width:100%">
+					<a href="http://ghardware.clanviquerat.ch/index.php?controller=Article&action=articlecommentaire&id=8"><img src="images/Slide2.png" style="width:100%"></a>
+				  <div class="text">2</div>
 				</div>
-
+			
+			<!-- Slide 3 -->
 				<div class="mySlides fade">
-				<a href="#2">
 				  <div class="numbertext">3 / 3</div>
-				  <img src="images/Slide3.png" style="width:100%">
-				 </a>
+					<a href="http://ghardware.clanviquerat.ch/index.php?controller=Article&action=articlecommentaire&id=16"><img src="images/Slide3.png" style="width:100%"></a>
+				  <div class="text">3</div>
 				</div>
 
-				</div>
-				<br>
+			
+			</div>
+			
+			<br>
 
-				<div style="text-align:center">
-				  <span class="dot"></span> 
-				  <span class="dot"></span> 
-				  <span class="dot"></span> 
-				</div>
+			
+			<!-- Slider Script -->
 
-				<script>
-				var slideIndex = 0;
+			<script>
+							var slideIndex = 0;
 				showSlides();
 
 				function showSlides() {
 					var i;
 					var slides = document.getElementsByClassName("mySlides");
-					var dots = document.getElementsByClassName("dot");
 					for (i = 0; i < slides.length; i++) {
-					   slides[i].style.display = "none";  
+						slides[i].style.display = "none"; 
 					}
 					slideIndex++;
-					if (slideIndex > slides.length) {slideIndex = 1}    
-					for (i = 0; i < dots.length; i++) {
-						dots[i].className = dots[i].className.replace(" active", "");
-					}
-					slides[slideIndex-1].style.display = "block";  
-					dots[slideIndex-1].className += " active";
+					if (slideIndex > slides.length) {slideIndex = 1} 
+					slides[slideIndex-1].style.display = "block"; 
 					setTimeout(showSlides, 2000); // Change image every 2 seconds
 				}
-				</script>
+			</script>
+			
+			<!-- Slider End -->
 
 			
 			

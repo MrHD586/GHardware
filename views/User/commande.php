17c426b1239f4bc6072b8fd4466fc18340540afd
togmar910 <33432619+togmar910@users.noreply.github.com>
2018-06-05 09:34:45 +0200

    commande

--- views/User/commande.php
+++ views/User/commande.php
@@ -3,45 +3,47 @@ $number=0;
 $Prix=0;
 $PrixTotal=0;
 foreach($order as $value){
-    
     if($number<=0){
-    echo 'Date de commande:'.$value['Date'].'</br>';
-    echo 'Numero commande:'.$value['NumberOrder'].'</br>';
-    if($value['State']==0){
+        foreach($user as $valueuser){
+            echo'Adresse:'.$valueuser['FirstName'].' '.$valueuser['LastName'].'</br>'.$valueuser['Road'].'</br>'.$valueuser['NPA'].' '.$valueuser['Town'].'</br>';
+        }
+        echo 'Date de commande:'.$value['Date'].'</br>';
+        echo 'Numero commande:'.$value['NumberOrder'].'</br>';
+        if($value['State']==0){
         
-        echo'Etat commande:'.'En attente du payement'.'<br>';
+            echo'Etat commande:'.'En attente du payement'.'<br>';
         
-    }else if($value['State']==1){
+        }else if($value['State']==1){
         
-        echo'Etat commande:'.'Envoyer'.'<br>';
+            echo'Etat commande:'.'Envoyer'.'<br>';
         
-    }else{
+        }else{
         
-        echo'Etat commande:'.'Arriver à destination'.'<br>';
+            echo'Etat commande:'.'Arriver à destination'.'<br>';
         
-    }
-    if($value['PayementMethod']==0){
+        }
+        if($value['PayementMethod']==0){
         
-        echo'Payement methode:'.'Nature'.'<br>';
+            echo'Payement methode:'.'Nature'.'<br>';
         
-    }else if($value['PayementMethod']==1){
+        }else if($value['PayementMethod']==1){
         
-        echo'Payement methode:'.'Facture'.'<br>';
+            echo'Payement methode:'.'Facture'.'<br>';
         
-    }
-    if($value['PayementState']==0){
+        }
+        if($value['PayementState']==0){
         
-        echo'Etat payement:'.'En cour de traitement'.'<br>';
+            echo'Etat payement:'.'En cour de traitement'.'<br>';
         
-    }else if($value['PayementState']==1){
+        }else if($value['PayementState']==1){
         
-        echo'Etat payement:'.'Payer'.'<br>';
+            echo'Etat payement:'.'Payer'.'<br>';
         
-    }else{
+        }else{
         
-        echo'Etat payement:'.'Payement refuser'.'<br>';
-    }
-    $number++;   
+            echo'Etat payement:'.'Payement refuser'.'<br>';
+        }
+        $number++;   
     }
     foreach($articles as $values){
         if(($value['Fk_Article'])==($values['idArticle'])){

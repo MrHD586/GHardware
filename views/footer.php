7a3169e4fd4174a4348dc42aadc8a7fb09f5a4f0
Valentin <vfavre@live.fr>
2018-06-05 14:16:51 +0200

    Correction Footer

--- views/footer.php
+++ views/footer.php
@@ -7,9 +7,11 @@
     ################################################################################
 	
 	echo '
-               <footer>
-	           <!-- End Row Central Part | DO NOT TOUCH -->
+               
             	</div> 
+				
+				<footer>
+	           <!-- End Row Central Part | DO NOT TOUCH -->
 	       <!-- FOOTER -->
         	<div class="row">
         		<div class="col-sm-4">
@@ -18,10 +20,7 @@
 				<a href="http://ghardware.clanviquerat.ch/Snake.php">Nos créations</a><br>
 				<a href="http://lmgtfy.com/?q=Comment+aller+se+faire+enculer">Nos adresses privées</a><br>
 				<a href="https://www.latlmes.com/breaking/ghardware-rvlation-de-2018-1">Nos exploits</a><br>
-				<a href="http://ghardware.clanviquerat.ch/rss.xml">
-                <img src="images/rss_logo.png" alt="Flux RSS" border="0" />
-                Flux RSS News
-                </a>
+				
 				</div>
         		<div class="col-sm-4"><a href="index.php?controller=Contact&action=Contact">Contact</a></div>
         	</div>

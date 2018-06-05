e356ba8fa036400d8426d57c273806efba1cc0a8
togmar910 <33432619+togmar910@users.noreply.github.com>
2018-06-05 20:37:08 +0200

    order

--- views/User/commande.php
+++ views/User/commande.php
@@ -149,4 +149,67 @@ echo'
 	<div class="showing">
 		<div class="col-lg-offset-4 col-sm-8">
 ';
+echo'
+	<div class="showing">
+		<div class="col-lg-offset-4 col-sm-8">
+';
+
+foreach($order as $value){
+    if($number<=0){
+        echo'<li>Adresse : '.$FirstName.' '.$LastName.'</br>'.$Road.'</br>'.$NPA.' '.$Town.'</br></li>';
+        echo '<li>Date de commande : '.$value['Date'].'</li>';
+        echo '<li>Numero commande : '.$value['NumberOrder'].'</li>';
+        if($value['State']==0){
+            
+            echo'<li>Etat commande : '.'En attente du payement'.'</li>';
+            
+        }else if($value['State']==1){
+            
+            echo'<li>Etat commande : '.'Envoyée'.'</li>';
+            
+        }else{
+            
+            echo'<li>Etat commande : '.'Arrivée à destination'.'</li>';
+            
+        }
+        if($value['PayementMethod']==0){
+            
+            echo'<li>Payement methode : '.'Nature'.'</li>';
+            
+        }else if($value['PayementMethod']==1){
+            
+            echo'<li>Payement methode : '.'Facture'.'</li>';
+            
+        }
+        if($value['PayementState']==0){
+            
+            echo'<li>Etat payement : '.'En cours de traitement'.'</li>';
+            
+        }else if($value['PayementState']==1){
+            
+            echo'<li>Etat payement : '.'Payé'.'</li>';
+            
+        }else{
+            
+            echo'<li>Etat payement : '.'Payement refuser'.'</li>';
+        }
+        $number++;
+    }
+    foreach($articles as $values){
+        if(($value['Fk_Article'])==($values['idArticle'])){
+            $Prix += $value['Number']*$values['Price'];
+            $PrixTotal += $value['Number']*$values['Price'];
+            echo '<li>Article : '.$values['Name'].'</li>';
+            echo '<li>Nombre commandé : '.$value['Number'].'</li>';
+            echo '<li>Prix:'.$Prix.'</li>';
+        }
+    }
+    $Prix=0;
+}
+echo'<br><strong><li>Total prix : '.$PrixTotal;
+
+echo'</strong></li></ul></div>
+	</div>';
+
+echo'</div>';
 

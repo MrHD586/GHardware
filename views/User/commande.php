fcc3cf519f71370541e2d73848a079cd109f25b3
Valentin <vfavre@live.fr>
2018-06-05 15:56:06 +0200

    li 2

--- views/User/commande.php
+++ views/User/commande.php
@@ -28,41 +28,41 @@ echo'
 foreach($order as $value){
     if($number<=0){
         echo'<li>Adresse : '.$FirstName.' '.$LastName.'</br>'.$Road.'</br>'.$NPA.' '.$Town.'</br></li>';
-        echo '<li>Date de commande : '.$value['Date'].'</li></br>';
-        echo '<li>Numero commande : '.$value['NumberOrder'].'</li></br>';
+        echo '<li>Date de commande : '.$value['Date'].'</li>';
+        echo '<li>Numero commande : '.$value['NumberOrder'].'</li>';
         if($value['State']==0){
         
-            echo'<li>Etat commande : '.'En attente du payement'.'</li><br>';
+            echo'<li>Etat commande : '.'En attente du payement'.'</li>';
         
         }else if($value['State']==1){
         
-            echo'<li>Etat commande : '.'Envoyer'.'</li><br>';
+            echo'<li>Etat commande : '.'Envoyer'.'</li>';
         
         }else{
         
-            echo'<li>Etat commande : '.'Arriver à destination'.'</li><br>';
+            echo'<li>Etat commande : '.'Arriver à destination'.'</li>';
         
         }
         if($value['PayementMethod']==0){
         
-            echo'<li>Payement methode : '.'Nature'.'</li><br>';
+            echo'<li>Payement methode : '.'Nature'.'</li>';
         
         }else if($value['PayementMethod']==1){
         
-            echo'<li>Payement methode : '.'Facture'.'</li><br>';
+            echo'<li>Payement methode : '.'Facture'.'</li>';
         
         }
         if($value['PayementState']==0){
         
-            echo'<li>Etat payement : '.'En cours de traitement'.'</li><br>';
+            echo'<li>Etat payement : '.'En cours de traitement'.'</li>';
         
         }else if($value['PayementState']==1){
         
-            echo'<li>Etat payement : '.'Payé'.'</li><br>';
+            echo'<li>Etat payement : '.'Payé'.'</li>';
         
         }else{
         
-            echo'<li>Etat payement : '.'Payement refuser'.'</li><br>';
+            echo'<li>Etat payement : '.'Payement refuser'.'</li>';
         }
         $number++;   
     }
@@ -70,9 +70,9 @@ foreach($order as $value){
         if(($value['Fk_Article'])==($values['idArticle'])){
             $Prix += $value['Number']*$values['Price'];
             $PrixTotal += $value['Number']*$values['Price'];
-            echo '<li>Article:'.$values['Name'].'</li></br>';
-            echo '<li>Nombre commandé:'.$value['Number'].'</li></br>';
-            echo '<li>Prix:'.$Prix.'</li></br>';
+            echo '<li>Article:'.$values['Name'].'</li>';
+            echo '<li>Nombre commandé:'.$value['Number'].'</li>';
+            echo '<li>Prix:'.$Prix.'</li>';
         }
     }
     $Prix=0;
